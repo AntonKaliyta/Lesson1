b8e718e23413314c52fa157a59f069fed4a69640
«anton» <kaluta99@gmail.com>
2023-02-09 06:33:57 +0700

    приветствие

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "тоха")
+{
+    System.Console.WriteLine("О, Тоха!");
+}
+else 
+{
+    System.Console.Write("Привет, ");
+    System.Console.Write(username);
+}
