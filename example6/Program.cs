c87b8ecc410b359f712cdf4067c4c0ad85b584fd
«anton» <kaluta99@gmail.com>
2023-02-09 06:45:21 +0700

    сравнение случайных числе

---
+++ example6/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random() .Next(1,101);
+int b = new Random() .Next(1,101);
+int c = new Random() .Next(1,101);
+int d = new Random() .Next(1,101);
+int e = new Random() .Next(1,101);
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
